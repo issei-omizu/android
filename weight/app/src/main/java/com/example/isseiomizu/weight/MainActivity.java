c8924d6201bb15f1b5b06e7373388475a4b0f6d8
issei omizu <p.solberg2003@gmail.com>
2016-09-06 18:09:55 +0900

    ・グラフを別Activityに切り出し ・リファクタリング

--- weight/app/src/main/java/com/example/isseiomizu/weight/MainActivity.java
+++ weight/app/src/main/java/com/example/isseiomizu/weight/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.isseiomizu.weight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.DatePickerDialog;
 import android.view.View.OnFocusChangeListener;
 import android.content.res.Configuration;
 
-import com.google.api.client.util.Value;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.api.client.extensions.android.http.AndroidHttp;
@@ -25,7 +23,6 @@ import com.google.api.services.sheets.v4.model.*;
 
 import android.Manifest;
 import android.accounts.AccountManager;
-import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -34,52 +31,29 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.text.TextUtils;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.graphics.Color;
-import android.graphics.Paint;
 
 import android.content.ContentValues;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Set;
 
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
-
-import org.achartengine.ChartFactory;
-import org.achartengine.GraphicalView;
-import org.achartengine.chart.BarChart;
-import org.achartengine.chart.PointStyle;
-import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
-import org.achartengine.renderer.XYMultipleSeriesRenderer;
-import org.achartengine.renderer.XYSeriesRenderer;
-import org.achartengine.model.TimeSeries;
 
 
 public class MainActivity extends AppCompatActivity
@@ -87,7 +61,6 @@ public class MainActivity extends AppCompatActivity
 
     GoogleAccountCredential mCredential;
     private TextView mOutputText;
-    private Button mCallApiButton;
     ProgressDialog mProgress;
 
     static final int REQUEST_ACCOUNT_PICKER = 1000;
@@ -102,12 +75,8 @@ public class MainActivity extends AppCompatActivity
 
     private final int HALF_DAY = 12 * 3600 * 1000;
 
-    private RelativeLayout mRlChart;
     private RelativeLayout mRlWeight;
 
-
-    private LinearLayout chartlayout;
-    private LinearLayout activityLayout;
 
     private Button mBtnImport;
     private Button mBtnExport;
@@ -133,15 +102,9 @@ public class MainActivity extends AppCompatActivity
         MyOpenHelper helper = new MyOpenHelper(this);
         this.mDbWeight = helper.getWritableDatabase();
 
-
-        this.mRlChart = (RelativeLayout) findViewById(R.id.rlChart);
         this.mRlWeight = (RelativeLayout) findViewById(R.id.rlWeight);
-
-        this.mRlChart.setVisibility(View.INVISIBLE);
         this.mRlWeight.setVisibility(View.VISIBLE);
 
-
-        this.mTextView = (TextView) findViewById(R.id.textView);
         this.editWeight = (EditText) findViewById(R.id.editWeight);
         this.editBodyFatPercentage = (EditText) findViewById(R.id.editBodyFatPercentage);
 
@@ -218,72 +181,18 @@ public class MainActivity extends AppCompatActivity
 
                 c.close();
 
-//                if (test != null) {
-//                    mRangeWrite = String.format("Sheet3!A%s:B%s", test.get(0), test.get(0));
-//
-//                    mTextView.setText(mDate + "::" + mRangeWrite);
-//
-//                    if (test.size() > 1) {
-//                        editWeight.setText(test.get(1).toString());
-//                        editBodyFatPercentage.setText(test.get(2).toString());
-//                    }
-//                }
             }
         });
 
-        chartlayout = (LinearLayout) findViewById(R.id.chart);
-//        GraphicalView graphicalView = TimeChartView();
-//        chartlayout.removeAllViews();
-//        chartlayout.addView(graphicalView);
-
-
-
-//        activityLayout = new LinearLayout(this);
-//        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-//                LinearLayout.LayoutParams.MATCH_PARENT,
-//                LinearLayout.LayoutParams.MATCH_PARENT);
-//        activityLayout.setLayoutParams(lp);
-//        activityLayout.setOrientation(LinearLayout.VERTICAL);
-//        activityLayout.setPadding(16, 16, 16, 16);
-//
-//        ViewGroup.LayoutParams tlp = new ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.WRAP_CONTENT,
-//                ViewGroup.LayoutParams.WRAP_CONTENT);
-//
-//        mCallApiButton = new Button(this);
-//        mCallApiButton.setText(BUTTON_TEXT);
-//        mCallApiButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                mCallApiButton.setEnabled(false);
-//                mOutputText.setText("");
-//                getResultsFromApi();
-//                mCallApiButton.setEnabled(true);
-//            }
-//        });
-//        activityLayout.addView(mCallApiButton);
-//
         mOutputText = new TextView(this);
-//        mOutputText.setLayoutParams(tlp);
-//        mOutputText.setPadding(16, 16, 16, 16);
-//        mOutputText.setVerticalScrollBarEnabled(true);
-//        mOutputText.setMovementMethod(new ScrollingMovementMethod());
-//        mOutputText.setText(
-//                "Click the \'" + BUTTON_TEXT +"\' button to test the API.");
-//        activityLayout.addView(mOutputText);
-//
+
         mProgress = new ProgressDialog(this);
         mProgress.setMessage("Calling Google Sheets API ...");
-//
-//        setContentView(activityLayout);
 
         // Initialize credentials and service object.
         mCredential = GoogleAccountCredential.usingOAuth2(
                 getApplicationContext(), Arrays.asList(SCOPES))
                 .setBackOff(new ExponentialBackOff());
-
-
-//        getResultsFromApi();
 
     }
 
@@ -293,20 +202,19 @@ public class MainActivity extends AppCompatActivity
         String orientation = config.orientation == Configuration.ORIENTATION_LANDSCAPE
                 ? "横" : "縦";
 
-        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            this.mRlChart.setVisibility(View.VISIBLE);
-            this.mRlWeight.setVisibility(View.INVISIBLE);
-
-            GraphicalView graphicalView = TimeChartView();
-            chartlayout.removeAllViews();
-            chartlayout.addView(graphicalView);
-        } else {
-            this.mRlChart.setVisibility(View.INVISIBLE);
-            this.mRlWeight.setVisibility(View.VISIBLE);
-        }
+//        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+//            this.mRlWeight.setVisibility(View.INVISIBLE);
+//        } else {
+//            this.mRlWeight.setVisibility(View.VISIBLE);
+//        }
 
         Toast.makeText(this, "Screenが" + orientation + "になりました",
                 Toast.LENGTH_LONG).show();
+    }
+
+    public void startSubActivity(View view){
+        Intent intent = new Intent(getApplication(), GraphActivity.class);
+        startActivity(intent);
     }
 
     private void setWriteDate() {
@@ -1020,154 +928,4 @@ public class MainActivity extends AppCompatActivity
             }
         }
     }
-
-    private GraphicalView TimeChartView() {
-        List<String> listDate = new ArrayList<>();
-        List<Double> listWeight = new ArrayList<>();
-        List<Double> listTargetWeight = new ArrayList<>();
-
-        String date = "";
-        String weight = "";
-        String bodyFatPercentage = "";
-
-        // (1)グラフデータの準備
-        // X軸 日付
-        String[] xStrValue = {"2015/12/22", "2015/12/21", "2015/12/20", "2015/12/18"};
-        // Y軸 体重
-        Double[] yDoubleValue = {71.2, 71.8, 70.8, 70.4};
-        // X軸 日付
-        String[] xStrValueTarget = {"2015/12/22", "2015/12/21", "2015/12/20", "2015/12/19", "2015/12/18"};
-        // Y軸 目標体重
-        Double[] yDoubleValueTarget = {70.3, 70.3, 70.3, 70.3, 70.3};
-
-        // sqliteからデータを全取得
-        Cursor c = mDbWeight.query("weight", new String[]{"date", "weight", "body_fat_percentage"}, null, null, null, null, "date DESC");
-        boolean mov = c.moveToFirst();
-
-        while (mov) {
-            date = c.getString(0);
-            weight = c.getString(1);
-            bodyFatPercentage = c.getString(2);
-
-            if (weight != null && !weight.isEmpty()) {
-                listDate.add(date);
-                listWeight.add(Double.parseDouble(weight));
-                listTargetWeight.add(55.0);
-            }
-
-            // 次のレコードへ
-            mov = c.moveToNext();
-        }
-
-        c.close();
-
-
-        xStrValue = listDate.toArray(new String[listDate.size()]);
-        xStrValueTarget = listDate.toArray(new String[listDate.size()]);
-        yDoubleValue = listWeight.toArray(new Double[listWeight.size()]);
-        yDoubleValueTarget = listTargetWeight.toArray(new Double[listTargetWeight.size()]);
-
-
-        // 日付を文字形式から Date型へ変換
-        int DataCount = xStrValue.length;
-        Date[] xDateValue = new Date[DataCount];
-        for (int i = 0; i < DataCount; i++) {
-            xDateValue[i] = String2date(xStrValue[i]);
-        }
-
-        int DataCountTarget = xStrValueTarget.length;
-        Date[] xDateValueTarget = new Date[DataCountTarget];
-        for (int i = 0; i < DataCountTarget; i++) {
-            xDateValueTarget[i] = String2date(xStrValueTarget[i]);
-        }
-
-        // (2) グラフのタイトル、X軸Y軸ラベル、色等の設定
-        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-        renderer.setChartTitle("体重");     // グラフタイトル
-        renderer.setChartTitleTextSize(25);             //
-        renderer.setXTitle("日付");                     // X軸タイトル
-        renderer.setYTitle("体重(Kg)");                     // Y軸タイトル
-        renderer.setAxisTitleTextSize(25);              //
-        renderer.setLegendTextSize(25);                 // 凡例　テキストサイズ
-        renderer.setPointSize(3f);                      // ポイントマーカーサイズ
-        renderer.setXAxisMin(xDateValue[xDateValue.length - 1].getTime() - HALF_DAY);  // X軸最小値
-//        renderer.setXAxisMax(xDateValue[0].getTime() + HALF_DAY);    // X軸最大値
-        renderer.setXAxisMax(xDateValue[xDateValue.length - 31].getTime() + HALF_DAY);    // X軸最大値
-        renderer.setYAxisMin(53.0f);                    // Y軸最小値
-        renderer.setYAxisMax(60.0f);                    // Y軸最大値
-        renderer.setXLabelsAlign(Paint.Align.CENTER);         // X軸ラベル配置位置
-        renderer.setYLabelsAlign(Paint.Align.RIGHT);          // Y軸ラベル配置位置
-        renderer.setAxesColor(Color.BLACK);            // X軸、Y軸カラー
-        renderer.setLabelsColor(Color.BLACK);          // ラベルカラー
-        renderer.setXLabelsColor(Color.BLACK);
-        renderer.setYLabelsColor(0, Color.BLACK);
-        renderer.setXLabels(31);                         // X軸ラベルのおおよその数
-        renderer.setYLabels(10);                         // Y軸ラベルのおおよその数
-        renderer.setLabelsTextSize(25);                 // ラベルサイズ
-        // グリッド表示
-        renderer.setShowGrid(true);
-        // グリッド色
-        renderer.setGridColor(Color.parseColor("#808080")); // グリッドカラー
-        // グラフ描画領域マージン top, left, bottom, right
-        renderer.setMargins(new int[]{40, 100, 15, 40});  //
-        // 凡例非表示
-        // renderer.setShowLegend(false);
-        renderer.setMarginsColor(Color.rgb(128, 128, 128));
-        // マージンを凡例にフィットさせる
-        renderer.setFitLegend(true);
-
-        // (3) データ系列の色、マーク等の設定
-        XYSeriesRenderer r1 = new XYSeriesRenderer();
-        r1.setColor(Color.rgb(255, 166, 0));
-        r1.setPointStyle(PointStyle.CIRCLE);
-        r1.setLineWidth(5f);
-        r1.setFillPoints(true);
-        renderer.addSeriesRenderer(r1);
-        XYSeriesRenderer r2 = new XYSeriesRenderer();
-        r2.setColor(Color.rgb(255, 0, 0));
-        r2.setPointStyle(PointStyle.SQUARE);
-        r2.setLineWidth(5f);
-        r2.setFillPoints(true);
-        renderer.addSeriesRenderer(r2);
-
-
-        // (4) データ系列　データの設定 (体重)
-        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-        TimeSeries series1 = new TimeSeries("体重");     // データ系列凡例
-        for (int i = 0; i < DataCount; i++) {
-            series1.add(xDateValue[i], yDoubleValue[i]);
-        }
-        dataset.addSeries(series1);
-
-        // (4) データ系列　データの設定 (目標値)
-        TimeSeries seriesTarget = new TimeSeries("目標値");     //
-        for (int i = 0; i < DataCountTarget; i++) {
-            seriesTarget.add(xDateValueTarget[i], yDoubleValueTarget[i]);
-        }
-        dataset.addSeries(seriesTarget);
-
-        // (5)タイムチャートグラフの作成
-        GraphicalView mChartView = ChartFactory.getTimeChartView(this, dataset, renderer, "M/d");
-
-        return mChartView;
-
-    }
-
-    /*
- * 日付時刻文字列を Date型に変換
- */
-    private Date String2date(String strDate) {
-        Date dateDate=null;
-        // 日付文字列→date型変換フォーマットを指定して
-        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyyMMdd");
-
-        try {
-            dateDate = sdf1.parse(strDate);
-        }
-        catch (ParseException e) {
-            Toast.makeText(this, "正しい日付を入力して下さい", Toast.LENGTH_SHORT).show();
-        }
-        return dateDate;
-    }
-
 }
